4965a368fd38c1edc9f0d93d9d6eae111ed17464
rtlgzn <75486094+rtlgzn@users.noreply.github.com>
2020-12-14 08:44:37 +0700

    Update sortshell.c

--- sortshell/sortshell.c
+++ sortshell/sortshell.c
@@ -4,25 +4,6 @@
 #include <conio.h>
 #include <time.h>
 
-struct timeval tv1,tv2,dtv;
-
-struct timezone tz;
-
-void time_start() 
-{ mingw_gettimeofday(&tv1, &tz); }
-
-long time_stop()
-{ mingw_gettimeofday(&tv2, &tz);
-
-  dtv.tv_sec= tv2.tv_sec -tv1.tv_sec;
-
-  dtv.tv_usec=tv2.tv_usec-tv1.tv_usec;
-
-  if(dtv.tv_usec<0) { dtv.tv_sec--; dtv.tv_usec+=1000000; }
-
-  return dtv.tv_sec*1000+dtv.tv_usec/1000;
-}
-
 void shell(int n, int mass[]){
 int i, j, step;
 int tmp;
@@ -40,16 +21,12 @@ for (step = n / 2; step > 0; step /= 2)
 }
 int main(){
     int n;
-    printf("input n: ");
     scanf("%d", &n);
     int* mass;
     mass = (int *)malloc(n * sizeof(int));
-    printf("input elements:\n");
         for (int i = 0; i < n; i++)
             scanf("%d", &mass[i]);
-            time_start();
             shell(n, mass);
-            printf("%ld", time_stop());
             for (int i = 0; i < n; i++)
                 printf("%d ", mass[i]);
             printf("\n");
